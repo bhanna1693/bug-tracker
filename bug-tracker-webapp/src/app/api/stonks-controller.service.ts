9a9abac27e349d8ad031b142764945a0c9c93fc7
brianhanna <bhanna1693@gmail.com>
2020-08-03 22:28:11 -0400

    getting ready to fetch global quote

--- bug-tracker-webapp/src/app/api/stonks-controller.service.ts
+++ bug-tracker-webapp/src/app/api/stonks-controller.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {environment} from "../../environments/environment";
 import {SymbolSearch} from "../models/stonks/symbol-search";
 import {CompanyOverview} from "../models/stonks/company-overview";
+import {GlobalQuote} from "../models/stonks/global-quote";
 
 @Injectable({
   providedIn: 'root'
@@ -24,6 +25,12 @@ export class StonksControllerService {
 
   getCompanyOverview(symbol: string) {
     return this.http.get<CompanyOverview>(environment.API_BASE_URL + '/stonks/company-overview', {
+      params: {symbol}
+    });
+  }
+
+  getGlobalQuote(symbol: string) {
+    return this.http.get<GlobalQuote>(environment.API_BASE_URL + '/stonks/global-quote', {
       params: {symbol}
     });
   }
