750fa9afe90ddc6c8b7b35fe5a9eb630eb597930
brianhanna <bhanna1693@gmail.com>
2020-08-27 21:19:06 -0400

    theme ready!

--- bug-tracker-webapp/src/app/theme/theme.module.ts
+++ bug-tracker-webapp/src/app/theme/theme.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {ThemeComponent} from './theme/theme.component';
 import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
   declarations: [ThemeComponent],
   imports: [
     CommonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatButtonModule
   ],
   exports: [ThemeComponent]
 })
